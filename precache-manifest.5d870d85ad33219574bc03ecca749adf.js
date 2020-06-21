b1670a3558a903add74ddc10665a7b2bd07d8b86
Ilya Sheremetov <ilya@sheremetov.com>
2020-06-21 22:33:32 +0200

    Updates

---
+++ precache-manifest.5d870d85ad33219574bc03ecca749adf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f85834daf692038f9aea2b0d345e7a44",
+    "url": "/Add__Note/index.html"
+  },
+  {
+    "revision": "ecb95482492b6b28b5d9",
+    "url": "/Add__Note/static/css/main.7d969135.chunk.css"
+  },
+  {
+    "revision": "8e21e6e08815c7a07db2",
+    "url": "/Add__Note/static/js/2.fa913b59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Add__Note/static/js/2.fa913b59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecb95482492b6b28b5d9",
+    "url": "/Add__Note/static/js/main.35add42b.chunk.js"
+  },
+  {
+    "revision": "f5830aa00c9e21fa4b27",
+    "url": "/Add__Note/static/js/runtime-main.0611126b.js"
+  }
+]);
